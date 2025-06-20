20b5ee42a5b99b2d6e565a3f421fc77716c0365d
Sintra <sintradacosta@icloud.com>
2025-06-20 22:14:06 +0100

    se añade el ejercicio 4

---
+++ ejercicio_4.js
@@ -0,0 +1,16 @@
+console.log("Ejercicio 4:");
+
+let number = 5;
+let factorial = 1;
+
+for (i= number; i>=1; i--){
+    factorial = factorial *i
+    
+}
+console.log(`El número factorial de ${number} es: ${factorial}`);
+
+
+
+
+
+
